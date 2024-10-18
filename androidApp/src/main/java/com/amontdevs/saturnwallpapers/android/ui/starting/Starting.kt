f3ce20b86a9378bd728f991d361db2590b351f82
Alexander M. <aleex1742@gmail.com>
2024-10-18 12:23:16 -0600

    loading animation

--- androidApp/src/main/java/com/amontdevs/saturnwallpapers/android/ui/starting/Starting.kt
+++ androidApp/src/main/java/com/amontdevs/saturnwallpapers/android/ui/starting/Starting.kt
@@ -10,18 +10,20 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.State
-import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalInspectionMode
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.airbnb.lottie.compose.LottieAnimation
+import com.airbnb.lottie.compose.LottieCompositionSpec
+import com.airbnb.lottie.compose.LottieConstants
+import com.airbnb.lottie.compose.animateLottieCompositionAsState
+import com.airbnb.lottie.compose.rememberLottieComposition
+import com.amontdevs.saturnwallpapers.android.R
 import com.amontdevs.saturnwallpapers.android.SaturnTheme
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -36,7 +38,23 @@ fun StartingScreen(
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.Center
         ) {
-            CircularProgressIndicator()
+            if (!LocalInspectionMode.current) {
+                val preloaderLottieComposition by rememberLottieComposition(
+                    LottieCompositionSpec.RawRes(R.raw.space_loading)
+                )
+                val preloaderProgress by animateLottieCompositionAsState(
+                    preloaderLottieComposition,
+                    iterations = LottieConstants.IterateForever,
+                    isPlaying = true
+                )
+                LottieAnimation(
+                    composition = preloaderLottieComposition,
+                    progress = { preloaderProgress },
+                    modifier = Modifier.height(350.dp)
+                )
+            } else {
+                CircularProgressIndicator()
+            }
             Spacer(modifier = Modifier.height(16.dp))
             Text(text = "${startingState.progress}%")
             Text(text = "Loading...")
